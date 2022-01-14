ce9907a7a506e352fd8cc8d38ceb83d126c1002f
David McLeish <david.mcleish@nearmap.com>
2022-01-14 14:43:11 +1100

    main.go: huffman code tokens

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 )
 
 func main() {
-	tokens := []string{"a", "a", "a", "a", "a", "a", "a", "b", "b", "b", "b", "c", "d", "e", "f"}
-	table := huffman.CreateHuffman(tokens)
-	fmt.Println(table)
-
 	points := []poly.Point{
 		{X: 0, Y: 0}, {X: 10, Y: 6}, {X: 7, Y: 11}, {X: -3, Y: 5},
 	}
+	var tokens []string
 	for i := range points {
-		fmt.Println(poly.Gradient(poly.Sub(points[i], points[(i+1)%len(points)])))
+		g := poly.Gradient(poly.Sub(points[i], points[(i+1)%len(points)]))
+		fmt.Println(g)
+		tokens = append(tokens, g...)
 	}
+	table := huffman.CreateHuffman(tokens)
+	fmt.Println(table)
+	// TODO: origin, scale
 }
