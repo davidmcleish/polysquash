f78d3a42a23c5bbb27de1ca906656b328252e67a
David McLeish <david.mcleish@nearmap.com>
2022-01-21 10:15:57 +1100

    WKT

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/davidmcleish/polysquash
 go 1.17
 
 require (
-	github.com/dgryski/go-bitstream v0.0.0-20180413035011-3522498ce2c8
 	github.com/juju/errors v0.0.0-20210818161939-5560c4c073ff
+	github.com/peterstace/simplefeatures v0.35.0
 )
+
+require github.com/dgryski/go-bitstream v0.0.0-20180413035011-3522498ce2c8 // indirect
--- polysquash/readwrite.go
+++ polysquash/readwrite.go
@@ -1,8 +1,42 @@
 package polysquash
 
-import "github.com/dgryski/go-bitstream"
+import (
+	"errors"
+	"io"
+	"io/ioutil"
 
-type Encoder interface {
-	Encode(w *bitstream.BitWriter, pts []Point) error
-	Decode(r *bitstream.BitReader) ([]Point, error)
+	"github.com/peterstace/simplefeatures/geom"
+)
+
+//import "github.com/dgryski/go-bitstream"
+
+type EncoderDecoder interface {
+	Encode(w io.Writer, poly geom.Polygon) error
+	Decode(r io.Reader) (*geom.Polygon, error)
+}
+
+type WKT struct{}
+
+func (t WKT) String() string { return "WKT" }
+
+func (t WKT) Encode(w io.Writer, poly geom.Polygon) error {
+	var wkt []byte
+	wkt = poly.AppendWKT(wkt)
+	_, err := w.Write(wkt)
+	return err
+}
+func (t WKT) Decode(r io.Reader) (*geom.Polygon, error) {
+	wkt, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	g, err := geom.UnmarshalWKT(string(wkt))
+	if err != nil {
+		return nil, err
+	}
+	poly, ok := g.AsPolygon()
+	if !ok {
+		return nil, errors.New("not a polygon George")
+	}
+	return &poly, nil
 }
