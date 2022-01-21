49228d25722b4c60beff47bc6c66617a024ade07
David McLeish <david.mcleish@nearmap.com>
2022-01-21 13:32:21 +1100

    circle

--- main.go
+++ main.go
@@ -18,17 +18,21 @@ func main() {
 		polysquash.Base64{Binary: polysquash.Offset{Precision: 1 << 24}},
 	}
 
-	polys := []string{
-		"POLYGON ((2000 3000, 2010 3006, 2007 3001, 1997 2995, 2000 3000))",
-		"POLYGON ((0 5, 0 6, 1 6, 1 7, 2 7, 2 8, 3 8, 3 9, 4 9, 4 10, 5 10, 5 9, 6 9, 6 8, 7 8, 7 7, 8 7, 9 6, 9 5, 10 5, 10 4, 9 4, 9 3, 8 3, 8 2, 7 2, 7 1, 6 1, 6 0, 5 0, 5 1, 4 1, 4 2, 3 2, 3 3, 2 3, 2 4, 1 4, 1 5, 0 5))",
-		stepPoly(151.196, -33.865, 0.00002, 0.00003, 100).AsText(),
-		randomStarPoly(-33.865, 151.196, 0.001, 100).AsText(),
+	polys := []struct {
+		name string
+		pstr string
+	}{
+		{"square", "POLYGON ((2000 3000, 2010 3006, 2007 3001, 1997 2995, 2000 3000))"},
+		{"small step", "POLYGON ((0 5, 0 6, 1 6, 1 7, 2 7, 2 8, 3 8, 3 9, 4 9, 4 10, 5 10, 5 9, 6 9, 6 8, 7 8, 7 7, 8 7, 9 6, 9 5, 10 5, 10 4, 9 4, 9 3, 8 3, 8 2, 7 2, 7 1, 6 1, 6 0, 5 0, 5 1, 4 1, 4 2, 3 2, 3 3, 2 3, 2 4, 1 4, 1 5, 0 5))"},
+		{"big step", stepPoly(151.196, -33.865, 0.00002, 0.00003, 100).AsText()},
+		{"star", starPoly(-33.865, 151.196, 0.001, 0.001, 100).AsText()},
+		{"circle", circlePoly(-33.865, 151.196, 0.001, 100).AsText()},
 	}
 
-	for i, pstr := range polys {
-		log.Println(i)
-		// log.Println(pstr)
-		g, _ := geom.UnmarshalWKT(pstr)
+	for _, tc := range polys {
+		log.Println(tc.name)
+		// log.Println(tc.pstr)
+		g, _ := geom.UnmarshalWKT(tc.pstr)
 		poly := g.MustAsPolygon()
 		for _, c := range candidates {
 			var buf bytes.Buffer
@@ -51,12 +55,16 @@ func main() {
 	}
 }
 
-func randomStarPoly(ox, oy float64, radius float64, npoints int) geom.Polygon {
+func circlePoly(ox, oy, radius float64, npoints int) geom.Polygon {
+	return starPoly(ox, oy, radius, 0, npoints)
+}
+
+func starPoly(ox, oy, inRadius, outRadius float64, npoints int) geom.Polygon {
 	r := rand.New(rand.NewSource(1234))
 	var pts []float64
 	for i := 0; i < npoints; i++ {
 		angle := math.Pi * 2 * (float64(i) / float64(npoints))
-		dist := (r.Float64()*0.5 + 0.5) * radius
+		dist := inRadius + r.Float64()*outRadius
 		x := ox + math.Cos(angle)*dist
 		y := oy + math.Sin(angle)*dist
 		pts = append(pts, x, y)
