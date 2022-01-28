80a7518f76e7ff985d4bccab7884cebef24c098a
David McLeish <david.mcleish@nearmap.com>
2022-01-28 16:08:54 +1100

    play around with different tests

--- main.go
+++ main.go
@@ -14,14 +14,16 @@ func main() {
 	candidates := []polysquash.EncoderDecoder{
 		// polysquash.WKT{},
 		// polysquash.Base64{Data: polysquash.Zip{Data: polysquash.WKT{}}},
-		// polysquash.Base64{Data: polysquash.Gzip{Data: polysquash.WKT{}}},
+		polysquash.Base64{Data: polysquash.Gzip{Data: polysquash.WKT{}}},
 		// polysquash.Base64{Data: polysquash.WKB{}},
+		polysquash.Base64{Data: polysquash.Gzip{Data: polysquash.WKB{}}},
 		// polysquash.Base64{Data: polysquash.HuffmanWKT{}},
 		polysquash.Base64{Data: polysquash.Offset{Precision: 1 << 24}},
-		polysquash.Base64{Data: polysquash.Huffman{Data: polysquash.Offset{Precision: 1 << 24}}},
+		// polysquash.Base64{Data: polysquash.Huffman{Data: polysquash.Offset{Precision: 1 << 24}}},
 		polysquash.Base64{Data: polysquash.Gzip{Data: polysquash.Offset{Precision: 1 << 24}}},
 		// polysquash.Base64{Data: polysquash.Gradient{Precision: 1 << 24}},
 		// polysquash.Base64{Data: polysquash.Geobuf{}},
+		polysquash.Base64{Data: polysquash.Gzip{Data: polysquash.Geobuf{}}},
 	}
 
 	polys := []struct {
